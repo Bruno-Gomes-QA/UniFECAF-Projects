e0cd9e26050163f89727985f7a6a4c0df4d5dbe3
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2025-09-12 08:38:48 -0300

    init

--- src/br/com/fecaf/Geometria.java
+++ src/br/com/fecaf/Geometria.java
@@ -5,9 +5,9 @@ import br.com.fecaf.controller.Menu;
 public class Geometria {
 
     public static void main(String[] args) {
-        System.out.println("/**************************************");
-        System.out.println("/*       Calculo de Geometria          *");
-        System.out.println("/**************************************");
+        System.out.println("/*****************************************");
+        System.out.println("/*              Geometria                *");
+        System.out.println("/*****************************************");
 
         Menu menu = new Menu();
         menu.executarMenu();
